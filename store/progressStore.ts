fd4dcf235a93897b4eb579d560e2dd00b09f4e6c
Izzat-Engida <izzatengida7@gmail.com>
2026-05-10 16:44:42 +0300

    progess store prepared

---
+++ store/progressStore.ts
@@ -0,0 +1,72 @@
+import {
+    getAnalytics,
+    getDailyProgress,
+    getWeeklyProgress,
+} from "@/services/progressService";
+import { Analytics, DailyProgress, WeeklyProgress } from "@/types/type";
+import { create } from "zustand";
+
+type ProgressState = {
+  dailyProgress: DailyProgress | null;
+  weeklyProgress: WeeklyProgress | null;
+  analytics: Analytics | null;
+  loading: boolean;
+  error: string | null;
+
+  loadDailyProgress: (childId: string, token: string) => Promise<void>;
+  loadWeeklyProgress: (childId: string, token: string) => Promise<void>;
+  loadAnalytics: (childId: string, token: string) => Promise<void>;
+};
+export const useProgressStore = create<ProgressState>((set) => ({
+  dailyProgress: null,
+  weeklyProgress: null,
+  analytics: null,
+  loading: false,
+  error: null,
+  loadDailyProgress: async (childId, token) => {
+    set({ loading: true });
+    try {
+      const data = await getDailyProgress(childId, token);
+      set({ dailyProgress: data, error: null });
+    } catch (error) {
+      set({
+        error:
+          error instanceof Error
+            ? error.message
+            : "Failed to load daily progress",
+      });
+    } finally {
+      set({ loading: false });
+    }
+  },
+  loadWeeklyProgress: async (childId, token) => {
+    set({ loading: true });
+    try {
+      const data = await getWeeklyProgress(childId, token);
+      set({ weeklyProgress: data, error: null });
+    } catch (error) {
+      set({
+        error:
+          error instanceof Error
+            ? error.message
+            : "Failed to load weekly progress",
+      });
+    } finally {
+      set({ loading: false });
+    }
+  },
+  loadAnalytics: async (childId, token) => {
+    set({ loading: true });
+    try {
+      const data = await getAnalytics(childId, token);
+      set({ analytics: data, error: null });
+    } catch (error) {
+      set({
+        error:
+          error instanceof Error ? error.message : "Failed to load analytics",
+      });
+    } finally {
+      set({ loading: false });
+    }
+  },
+}));
